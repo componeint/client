8be944913b21eb8d9e684b368ebfacf1d090b7cd
anonymoussc <50c5ac69@opayq.com>
2016-02-28 13:44:14 +0700

    Modified states

--- resources/angular-material/states.js
+++ resources/angular-material/states.js
@@ -101,7 +101,64 @@
                         controllerAs: 'landing'
                     }
                 }
+            })
+            .state('profile', {
+                abstract: true,
+                url     : '/profile',
+                views   : {
+                    'layout@'        : {
+                        templateUrl : layoutProvider.layout('minimalist.theme'),
+                        controller  : 'ProfileController',
+                        controllerAs: 'profile'
+                    },
+                    'header@profile' : {
+                        templateUrl : layoutProvider.layout('minimalist.header'),
+                        controller  : 'HeaderController',
+                        controllerAs: 'header'
+                    },
+                    'sidenav@profile': {
+                        templateUrl : layoutProvider.layout('minimalist.sidenav'),
+                        controller  : 'SidenavController',
+                        controllerAs: 'sidenav'
+                    },
+                    'aside@profile'  : {
+                        templateUrl : layoutProvider.layout('minimalist.aside'),
+                        controller  : 'AsideController',
+                        controllerAs: 'aside'
+                    },
+                    'footer@profile' : {
+                        templateUrl : layoutProvider.layout('minimalist.footer'),
+                        controller  : 'FooterController',
+                        controllerAs: 'footer'
+                    },
+                    'main@profile'   : {}
+                }
+            })
+            .state('profile.user', {
+                url    : '/{user}',
+                data   : {pageName: 'Profile'},
+                views  : {
+                    'main@profile': {
+                        templateUrl : layoutProvider.view('profile.username'),
+                        controller  : 'ProfileUsernameController',
+                        controllerAs: 'profileUser'
+                    }
+                },
+                resolve: {
+                    loginRequired: loginRequired
+                }
             });
+
+        function loginRequired($q, $state, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.resolve();
+            } else {
+                $state.go('jwtauth.signin');
+            }
+
+            return deferred.promise;
+        }
 
     }
 
