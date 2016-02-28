dec7e12cc16cdeae9da0cf8a1d18eab0cce629cf
anonymoussc <50c5ac69@opayq.com>
2016-02-28 13:45:25 +0700

    Add profile

---
+++ resources/angular-material/app/profile/pic/pic.controller.js
@@ -0,0 +1,9 @@
+/**
+ * pic.controller
+ * Created by anonymous on 27/02/16 6:45.
+ */
+
+angular.module('cardDemo1', ['ngMaterial'])
+    .controller('AppCtrl', function($scope) {
+        $scope.imagePath = 'img/washedout.png';
+    });
