839639cabd232a40f69a91aab0a554d9be1f8ad1
anonymoussc <50c5ac69@opayq.com>
2016-02-28 19:44:21 +0700

    Add landing

---
+++ resources/angular-material/app/client/landing/landing.controller.js
@@ -0,0 +1,31 @@
+/**
+ * landing.controller.js
+ * Created by anonymous on 28/02/16 19:28.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ClientLandingController', ClientLandingController);
+
+    ClientLandingController.$inject = [];
+
+    /* @ngInject */
+    function ClientLandingController() {
+
+        var vm   = this;
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
