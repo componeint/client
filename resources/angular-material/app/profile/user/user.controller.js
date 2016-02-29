a90f3ee437d08678487541b8ef64a4bdc4b63c20
anonymoussc <50c5ac69@opayq.com>
2016-02-29 18:55:54 +0700

    Updated

--- resources/angular-material/app/profile/user/user.controller.js
+++ resources/angular-material/app/profile/user/user.controller.js
@@ -1,5 +1,5 @@
 /**
- * username.controller.js
+ * user.controller.js
  * Created by anonymous on 27/02/16 1:55.
  */
 
@@ -18,44 +18,16 @@
         var vm       = this;
         var tabs     = [
                 {
-                    title  : 'One',
+                    title  : 'Posts',
                     content: 'Tabs will become paginated if there isn\'t enough room for them.'
                 },
                 {
-                    title  : 'Two',
+                    title  : 'Profile',
                     content: 'You can swipe left and right on a mobile device to change tabs.'
                 },
                 {
-                    title  : 'Three',
+                    title  : 'Store',
                     content: 'You can bind the selected tab via the selected attribute on the md-tabs element.'
-                },
-                {
-                    title  : 'Four',
-                    content: 'If you set the selected tab binding to -1, it will leave no tab selected.'
-                },
-                {
-                    title  : 'Five',
-                    content: 'If you remove a tab, it will try to select a new one.'
-                },
-                {
-                    title  : 'Six',
-                    content: 'There\'s an ink bar that follows the selected tab, you can turn it off if you want.'
-                },
-                {
-                    title  : 'Seven',
-                    content: 'If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab.'
-                },
-                {
-                    title  : 'Eight',
-                    content: 'If you look at the source, you\'re using tabs to look at a demo for tabs. Recursion!'
-                },
-                {
-                    title  : 'Nine',
-                    content: 'If you set md-theme="green" on the md-tabs element, you\'ll get green tabs.'
-                },
-                {
-                    title  : 'Ten',
-                    content: 'If you\'re still reading this, you should just go check out the API docs for tabs!'
                 }
             ],
             selected = null,
